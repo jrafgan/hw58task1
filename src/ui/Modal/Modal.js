ddd392de02d24e933d001dc0aeaf15275c8078fa
Maksat Duishenaliev <maksatovy@gmail.com>
2019-01-14 14:41:45 +0600

    there is 2 buttons generating iside this button

--- src/ui/Modal/Modal.js
+++ src/ui/Modal/Modal.js
@@ -1,25 +1,30 @@
 import React, {Fragment} from 'react';
 import Backdrop from "../Backdrop/Backdrop";
 import './Modal.css';
+import Button from "../Button/Button";
 
 
 const Modal = props => {
-  return (
-    <Fragment>
-      <Backdrop onClick={props.close} show={props.show} />
-      <div
-        className="Modal"
-        style={{
-          transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-          opacity: props.show ? '1' : '0'
-        }}>
-          <div className="modalTitle">{props.title}
-          <img alt="btn" src={props.imgUrl} className="dltImage" onClick={props.closed}/>
-          </div>
-          <div className="modalText">{props.text}</div>
-      </div>
-    </Fragment>
-  );
+    return (
+        <Fragment>
+            <Backdrop onClick={props.close} show={props.show}/>
+            <div
+                className="Modal"
+                style={{
+                    transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                    opacity: props.show ? '1' : '0'
+                }}>
+                <div className="modalTitle">{props.title}</div>
+                <div className="modalText">{props.text}</div>
+                <div className="btnDiv">
+                    {props.config.map((item, id) => {
+                        return <Button onClick={item.clicked} key={id} btnType={item.type} label={item.label}
+                                       show={props.show}/>
+                    })}
+                </div>
+            </div>
+        </Fragment>
+    );
 };
 
 export default Modal;
